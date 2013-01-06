c6beb8cb6976179169a58a029c09c7f7eeff2135
jjnetconn/n/n <jj@netconn.dk/n/n>
2013-01-06 09:41:03 +0100

    Excel InterOp Hanging solved with Marshal object closeing

--- WindowsFormsApplication1/ExcelLoad.cs
+++ WindowsFormsApplication1/ExcelLoad.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Data;
-using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+using Marshal = System.Runtime.InteropServices.Marshal;
+
 using System.Reflection;
 
 namespace ServiceOverblik
@@ -12,13 +14,15 @@ namespace ServiceOverblik
     {
         public DataSet GetExcel(string fileName)
         {
-            Application oXL;
-            Workbook oWB;
-            Worksheet oSheet;
-            Range oRng;
-
+           
+                Excel.Application oXL = null;
+                Excel.Workbook oWB = null;
+                Excel.Worksheet oSheet = null;
+                Excel.Range oRng = null;
+            try
+            {
                 //  creat a Application object
-                oXL = new Application();
+                oXL = new Excel.Application();
                 //   get   WorkBook  object
                 oWB = oXL.Workbooks.Open(fileName, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value,
                         Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value,
@@ -26,8 +30,8 @@ namespace ServiceOverblik
 
 
                 //   get   WorkSheet object 
-                oSheet = (Microsoft.Office.Interop.Excel.Worksheet)oWB.Sheets[1];
-                System.Data.DataTable dt = new System.Data.DataTable("dtExcel");
+                oSheet = (Excel.Worksheet)oWB.Sheets[1];
+                DataTable dt = new DataTable("dtExcel");
                 DataSet ds = new DataSet();
                 ds.Tables.Add(dt);
                 DataRow dr;
@@ -60,8 +64,72 @@ namespace ServiceOverblik
                     ds.Tables["dtExcel"].Rows.Add(dr);
                 }
                 return ds;
+            }
+            finally
+            {
+                // Release all COM RCWs.
+                // The "releaseObject" will just "do nothing" if null is passed,
+                // so no need to check to find out which need to be released.
+                // The "finally" is run in all cases, even if there was an exception
+                // in the "try". 
+                // Note: passing "by ref" so afterwords "xlWorkSheet" will
+                // evaluate to null. See "releaseObject".
+                releaseObject(ref oSheet);
+                releaseObject(ref oWB);
+                // The Quit is done in the finally because we always
+                // want to quit. It is no different than releasing RCWs.
+                if (oXL != null)
+                {
+                    oXL.Quit();
+                }
+                releaseObject(ref oXL);
+            }
           
-          
+        }
+
+        private void releaseObject(ref Excel.Application obj)
+        {
+            // Do not catch an exception from this.
+            // You may want to remove these guards depending on
+            // what you think the semantics should be.
+            if (obj != null && Marshal.IsComObject(obj))
+            {
+                Marshal.ReleaseComObject(obj);
+            }
+            // Since passed "by ref" this assingment will be useful
+            // (It was not useful in the original, and neither was the
+            //  GC.Collect.)
+            obj = null;
+        }
+
+        private void releaseObject(ref Excel.Workbook obj)
+        {
+            // Do not catch an exception from this.
+            // You may want to remove these guards depending on
+            // what you think the semantics should be.
+            if (obj != null && Marshal.IsComObject(obj))
+            {
+                Marshal.ReleaseComObject(obj);
+            }
+            // Since passed "by ref" this assingment will be useful
+            // (It was not useful in the original, and neither was the
+            //  GC.Collect.)
+            obj = null;
+        }
+
+        private void releaseObject(ref Excel.Worksheet obj)
+        {
+            // Do not catch an exception from this.
+            // You may want to remove these guards depending on
+            // what you think the semantics should be.
+            if (obj != null && Marshal.IsComObject(obj))
+            {
+                Marshal.ReleaseComObject(obj);
+            }
+            // Since passed "by ref" this assingment will be useful
+            // (It was not useful in the original, and neither was the
+            //  GC.Collect.)
+            obj = null;
         }
     }
 }
